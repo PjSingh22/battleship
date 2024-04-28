cba09226b99d2a8d46f54b37be0f20a2ad01980e
Prabhjot Singh <pjsingh1359@gmail.com>
2024-04-27 17:41:59 -0700

    create ship class

---
+++ app/Ship.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Ship() {
+  const [length, setLength] = useState(0)
+  const [orientation, setOrientation] = useState('horizontal')
+  const [numberOfHits, setNumberOfHits] = useState(0)
+  const [isSunk, setIsSunk] = useState(false)
+
+  return (
+    <></>
+  )
+}
+
+export default Ship
